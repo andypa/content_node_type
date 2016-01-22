3a2d10a8056851840813f7a5bde4e2b0e20ad87a
Juan Manuel Vergés Solanas <juanmanuel.vergessolanas@gmail.com>
2016-01-22 14:43:25 +0100

    [TASK] Moved key and hide to first tab, added switch instead of checkbox

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -6,21 +6,29 @@ if (!defined('TYPO3_MODE')) {
 //$GLOBALS['TCA']['tt_content']['ctrl']['formattedLabel_userFunc'] = \SimpleTYPO3\ContentNodeType\Hooks\Labels::class . '->propertiesBasedTitle';
 //
 //$GLOBALS['TCA']['tt_content']['ctrl']['label_userFunc'] = \SimpleTYPO3\ContentNodeType\Hooks\Labels::class . '->propertiesBasedTitle';
+//--palette--;;key_line,
+$GLOBALS['TCA']['tt_content']['palettes'] = \FluidTYPO3\Flux\Utility\RecursiveArrayUtility::mergeRecursiveOverrule(
+	$GLOBALS['TCA']['tt_content']['palettes'],
+	[
+		'key_line' => [
+			'showitem' => 'header;Content Element Key,hidden;Hide this element?',
+		]
+	]
+);
 
 $common = '
         --div--;Common Properties,
                 --palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.frames;frames,
         --div--;Content Node Properties,
                 --palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.general;general,
-		        --palette--;Content element Key;headers,
         --div--;LLL:EXT:cms/locallang_ttc.xlf:tabs.access,
-                --palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.visibility;visibility,
                 --palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.access;access,
         --div--;LLL:EXT:cms/locallang_ttc.xlf:tabs.extended
         ';
 
 $GLOBALS['TCA']['tt_content']['types']['list']['showitem'] = '
         --div--;Properties,
+				--palette--;;key_line,
                 list_type;LLL:EXT:cms/locallang_ttc.xlf:list_type_formlabel,
                 select_key;LLL:EXT:cms/locallang_ttc.xlf:select_key_formlabel,
                 pages;LLL:EXT:cms/locallang_ttc.xlf:pages.ALT.list_formlabel,
@@ -30,23 +38,27 @@ $GLOBALS['TCA']['tt_content']['types']['list']['showitem'] = '
 
 $GLOBALS['TCA']['tt_content']['types']['fluidcontent_content']['showitem'] = '
         --div--;Properties,
+                --palette--;;key_line,
                 pi_flexform,
 '.$common;
 
 
 $GLOBALS['TCA']['tt_content']['types']['header']['showitem'] = '
 		--div--;Properties,
+				--palette--;;key_line,
                 --palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.headers;headers,
 '.$common;
 
 $GLOBALS['TCA']['tt_content']['types']['text']['showitem'] = '
 		--div--;Properties,
+				--palette--;;key_line,
                 bodytext;LLL:EXT:cms/locallang_ttc.xlf:bodytext_formlabel;;richtext:rte_transform[flag=rte_enabled|mode=ts_css],
                 rte_enabled;LLL:EXT:cms/locallang_ttc.xlf:rte_enabled_formlabel,
 '.$common;
 
 $GLOBALS['TCA']['tt_content']['types']['image']['showitem'] = '
 		--div--;Properties,
+				--palette--;;key_line,
                 image,
                 --palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.imagelinks;imagelinks,
 
@@ -54,11 +66,13 @@ $GLOBALS['TCA']['tt_content']['types']['image']['showitem'] = '
 
 $GLOBALS['TCA']['tt_content']['types']['bullets']['showitem'] = '
 		--div--;Properties,
+				--palette--;;key_line,
                 bodytext;LLL:EXT:cms/locallang_ttc.xlf:bodytext.ALT.bulletlist_formlabel;;nowrap,
 '.$common;
 
 $GLOBALS['TCA']['tt_content']['types']['table']['showitem'] = '
 		--div--;Properties,
+				--palette--;;key_line,
                 --palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.table_layout;tablelayout,
                 layout;;10,
                 cols,
@@ -68,18 +82,21 @@ $GLOBALS['TCA']['tt_content']['types']['table']['showitem'] = '
 
 $GLOBALS['TCA']['tt_content']['types']['uploads']['showitem'] = '
 		--div--;Properties,
+				--palette--;;key_line,
                 --palette--;LLL:EXT:cms/locallang_ttc.xlf:media;uploads,
                 --palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.uploads_layout;uploadslayout,
 '.$common;
 
 $GLOBALS['TCA']['tt_content']['types']['menu']['showitem'] = '
 		--div--;Properties,
+				--palette--;;key_line,
                 --palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.menu;menu,
                 --palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.menu_accessibility;menu_accessibility,
 '.$common;
 
 $GLOBALS['TCA']['tt_content']['types']['shortcut']['showitem'] = '
 		--div--;Properties,
+				--palette--;;key_line,
                 header;LLL:EXT:cms/locallang_ttc.xlf:header.ALT.shortcut_formlabel,
                 records;LLL:EXT:cms/locallang_ttc.xlf:records_formlabel,
 '.$common;
@@ -87,11 +104,11 @@ $GLOBALS['TCA']['tt_content']['types']['shortcut']['showitem'] = '
 
 $GLOBALS['TCA']['tt_content']['types']['div']['showitem'] = '
 		--div--;Properties,
-                header;LLL:EXT:cms/locallang_ttc.xlf:header.ALT.div_formlabel,
+				--palette--;;key_line,
 '.$common;
 
 $GLOBALS['TCA']['tt_content']['types']['html']['showitem'] = '
 		--div--;Properties,
-                header;LLL:EXT:cms/locallang_ttc.xlf:header.ALT.html_formlabel,
+				--palette--;;key_line,
                 bodytext,
 '.$common;
--- ext_tables.php
+++ ext_tables.php
@@ -7,3 +7,13 @@ if (!defined('TYPO3_MODE')) {
 if ('BE' === TYPO3_MODE) {
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript($_EXTKEY, 'setup', '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:content_node_type/Configuration/TypoScript/setup.txt">');
 }
+
+
+$columns = [
+	'hidden',
+];
+foreach ($columns as $column) {
+	$GLOBALS['TCA']['tt_content']['columns'][$column]['config']['renderType'] = 'bootstrapSwitchElement';
+	$GLOBALS['TCA']['tt_content']['columns'][$column]['config']['type'] = 'input';
+	unset($GLOBALS['TCA']['tt_content']['columns'][$column]['config']['items']);
+}
